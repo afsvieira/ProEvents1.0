4634284c6c35c86331eabb42051ed1e0f9564cab
antoniov <antoniov@sonele.com>
2023-04-01 13:24:26 -0400

    Section 5: Done

--- Front/ProEventos-App/src/app/events/events.component.ts
+++ Front/ProEventos-App/src/app/events/events.component.ts
@@ -8,10 +8,28 @@ import { Component } from '@angular/core';
 })
 export class EventsComponent {
 
+  public eventsFiltered: any = [];
   public events: any = [];
   widthImg = 150;
   marginImg = 2;
   showImg = true;
+  private _listFilter: string = '';
+
+  public get listFilter(): string{
+    return this._listFilter;
+  }
+  public set listFilter(value: string){
+    this._listFilter = value;
+    this.eventsFiltered = this.listFilter? this.filterEvents(this.listFilter) : this.events;
+  }
+
+  filterEvents(filterBy: string): any{
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.events.filter(
+      (event: any) => event.subject.toLocaleLowerCase().indexOf(filterBy) !== -1 ||
+      event.local.toLocaleLowerCase().indexOf(filterBy) !== -1
+    )
+  }
 
   constructor(private http: HttpClient){ }
 
@@ -21,7 +39,10 @@ export class EventsComponent {
 
   public getEvents(): void {
     this.http.get('https://localhost:5001/api/Events').subscribe(
-      response => this.events = response,
+      response => {
+        this.events = response,
+        this.eventsFiltered = this.events
+      },
       error => console.log(error),
     );
   }
